3df178efa32c8d306a327be7a78519ab1300cf9d
Pratik More <developer@pratikmore.com>
2023-05-16 23:17:18 -0400

    Bug issue : 1
    To solve the bug this code is commented , However we can still use this code later. The error is when user try to login or register and clicks on username or password field it automatically redirects to welcome.blade.php
    
    Possible error: it might be because of cache and we can clear it using this commands
    1) php artisan cache:clear (Clear Cache)
    2) php artisan route:clear (Clear Routes)
    3) php artisan config:clear (Clear Config) NOTE this can't be used after building complex structure
    4)  php artisan view:clear

--- WebgemApp1/routes/web.php
+++ WebgemApp1/routes/web.php
@@ -18,18 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*Route::get('/dashboard', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');*/
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::post('/login', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
