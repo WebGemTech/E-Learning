92fb6fba4a77e28de5808af35419110048580ecd
Shubham Gupta <shubham.gupta.webgem@outlook.com>
2023-06-15 13:11:51 +0530

    Register, Login. Forgot Password
    Logo Updated in all the pages

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -30,8 +30,8 @@
                         <div class="text-center mt-4">
                             <div class="mb-3">
                                 <a href="index.html" class="auth-logo">
-                                    <img src="{{ asset('backend/assets/images/logo-dark.png') }}" height="30" class="logo-dark mx-auto" alt="">
-                                    <img src="{{ asset('backend/assets/images/logo-light.png') }}" height="30" class="logo-light mx-auto" alt="">
+                                    <img src="https://pratikmore.com/assets/img/logo.png" height="30" class="logo-dark mx-auto" alt="">
+                                    <img src="https://pratikmore.com/assets/img/logo.png" height="30" class="logo-light mx-auto" alt="">
                                 </a>
                             </div>
                         </div>
