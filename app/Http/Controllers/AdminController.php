b1d02671e0f51ffef54f8bb4ed87d7d66a60024a
Shubham Gupta <shubham.gupta.webgem@outlook.com>
2023-06-26 12:03:50 +0530

    Dashboard > Profile
    Profile page created and new file created in admin folder "admin_profile_view.blade.php"

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -20,4 +21,12 @@ class AdminController extends Controller
 
         return redirect('/login');
     } // End method
+
+    public function Profile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+    }// End Method
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::middleware('auth')->group(function () {
 // Admin route
 Route::controller(AdminController::class)->group(function(){
     Route::get('/admin/logout','destroy')->name('admin.logout');
+    Route::get('/admin/profile', 'Profile')->name('admin.profile');
 
 });
 
