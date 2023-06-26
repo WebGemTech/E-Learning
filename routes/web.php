6ee8e397e872d8e26c2fec563efa58f5c85ff495
Shubham Gupta <shubham.gupta.webgem@outlook.com>
2023-06-27 00:51:24 +0530

    Edit Profile Button
    New file created - resources>> views>> admin>> admin_profile_edit.blade.php
    Database new field added

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::middleware('auth')->group(function () {
 Route::controller(AdminController::class)->group(function(){
     Route::get('/admin/logout','destroy')->name('admin.logout');
     Route::get('/admin/profile', 'Profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
 
 });
 
