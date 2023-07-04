9e0d49340c6eafdc72fcfdf367bcc77c5223a8bb
Shubham Gupta <shubham.gupta.webgem@outlook.com>
2023-07-04 19:12:25 +0530

    Change Password
    Change password page created

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -65,4 +65,16 @@ class AdminController extends Controller
         return redirect()->route('admin.profile')->with($notification);
 
     }// End Method
+
+    public function ChangePassword(){
+
+        return view('admin.admin_change_password');
+
+    }// End Method
+
+
+    public function UpdatePassword(Request $request){
+
+    }// End Method
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::controller(AdminController::class)->group(function(){
     Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
     Route::post('/store/profile', 'StoreProfile')->name('store.profile');
 
+    Route::get('/change/password', 'ChangePassword')->name('change.password');
+    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+
 });
 
 require __DIR__.'/auth.php';
